95c67d1a615dd59a62ccf31c6629e2b5172bcc68
J0sh0nat0r <jojo.parillon@gmail.com>
2022-04-26 02:31:17 +0100

    feat: add RelationTest for serialization

---
+++ tests/RelationTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace J0sh0nat0r\Oso\Tests;
+
+use J0sh0nat0r\Oso\Relation;
+
+it('serializes "one" relationships correctly', function () {
+    $relation = Relation::one(
+        'Foo',
+        'foo_id',
+        'id'
+    );
+
+    expect($relation->jsonSerialize())->toBe([
+        'Relation' => [
+            'kind' => 'one',
+            'other_class_tag' => 'Foo',
+            'my_field' => 'foo_id',
+            'other_field' => 'id'
+        ]
+    ]);
+});
+
+it('serializes "many" relationships correctly', function () {
+    $relation = Relation::many(
+        'Bar',
+        'id',
+        'foo_id'
+    );
+
+    expect($relation->jsonSerialize())->toBe([
+        'Relation' => [
+            'kind' => 'many',
+            'other_class_tag' => 'Bar',
+            'my_field' => 'id',
+            'other_field' => 'foo_id'
+        ]
+    ]);
+});
